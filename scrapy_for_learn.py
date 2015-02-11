3af47cc880013705352a66cc5c3fba7dbc66603a
SamDeng <dgaosheng@gmail.com>
2015-02-12 00:33:06 +0800

    modifly test file

--- scrapy_for_learn.py
+++ scrapy_for_learn.py
@@ -9,6 +9,13 @@ def gethtml(url):
     return html
 
 def getimg(html):
-    reg = r'src=
+    reg = r'src="(.+?\.jpg)" pic_ext' #
+    imgre = re.compile(reg)
+    imglist = re.findall(imgre, html)
+    return imglist
+
+html = gethtml("http://www.taobao.com/market/nvbao/shouye.php?spm=1.7274553.201.31.TRCyFR")
+print getimg(html)
+
 
 
