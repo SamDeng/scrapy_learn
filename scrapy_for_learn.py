e056b3f0ac8fca117aa270a0cee8f2a9b57d7ed5
SamDeng <dgaosheng@gmail.com>
2015-02-11 22:13:45 +0800

    add some files

--- scrapy_for_learn.py
+++ scrapy_for_learn.py
@@ -1,6 +1,14 @@
-from scrapy.spider import BaseSpider
+#http://www.cnblogs.com/fnng/p/3576154.html
+
+import urllib
+import re
+
+def gethtml(url):
+    page = urllib.urlopen(url)
+    html = page.read()
+    return html
+
+def getimg(html):
+    reg = r'src=
 
 
-class demo(BaseSpider):
-    name = "dmoz"
-    allow_domain = ["dmoz.org"]
